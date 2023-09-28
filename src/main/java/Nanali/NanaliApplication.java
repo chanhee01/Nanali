b874ec7995fd14739a59d7e402e8744cd88ef857
chanhee01 <joch4267@naver.com>
2023-09-28 15:35:26 +0900

    first commit

---
+++ src/main/java/Nanali/NanaliApplication.java
@@ -0,0 +1,13 @@
+package Nanali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Nanali/Nanali/NanaliApplicationTests.java
@@ -0,0 +1,13 @@
+package Nanali.Nanali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
