6e3bc0a220d3bafd3c0db236503683983a9d3eaf
chanhee01 <joch4267@naver.com>
2023-10-28 21:04:15 +0900

    [Update] 날씨 조건에 맞는 옷을 select하는 쿼리 추가

--- src/main/java/Nanali/repository/GarmentRepository.java
+++ src/main/java/Nanali/repository/GarmentRepository.java
@@ -2,6 +2,27 @@ package Nanali.repository;
 
 import Nanali.domain.cody.cloth.Garment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GarmentRepository extends JpaRepository<Garment, Long> {
+
+    @Query("select g from Garment g where g.temp_from < :temp and g.temp_to > :temp and g.uv_from < :uv " +
+            "and g.uv_to > :uv and g.rain_from < :rain and g.rain_to > : rain and g.category = 'outer'")
+    List<Garment> findAllByCategoryIsOuter(@Param(value = "temp") Long temp, @Param(value = "uv") Long uv, @Param(value = "rain") Long rain);
+
+    @Query("select g from Garment g where g.temp_from < :temp and g.temp_to > :temp and g.uv_from < :uv " +
+            "and g.uv_to > :uv and g.rain_from < :rain and g.rain_to > : rain and g.category = 'top'")
+    List<Garment> findAllByCategoryIsTop(@Param(value = "temp") Long temp, @Param(value = "uv") Long uv, @Param(value = "rain") Long rain);
+
+    @Query("select g from Garment g where g.temp_from < :temp and g.temp_to > :temp and g.uv_from < :uv " +
+            "and g.uv_to > :uv and g.rain_from < :rain and g.rain_to > : rain and g.category = 'pants'")
+    List<Garment> findAllByCategoryIsPants(@Param(value = "temp") Long temp, @Param(value = "uv") Long uv, @Param(value = "rain") Long rain);
+
+    @Query("select g from Garment g where g.temp_from < :temp and g.temp_to > :temp and g.uv_from < :uv " +
+            "and g.uv_to > :uv and g.rain_from < :rain and g.rain_to > : rain and g.category = 'shoes'")
+    List<Garment> findAllByCategoryIsShoes(@Param(value = "temp") Long temp, @Param(value = "uv") Long uv, @Param(value = "rain") Long rain);
+
 }
--- src/main/java/Nanali/repository/OutfitRepository.java
+++ src/main/java/Nanali/repository/OutfitRepository.java
@@ -1,7 +1,16 @@
 package Nanali.repository;
 
+import Nanali.domain.cody.cloth.Garment;
 import Nanali.domain.cody.cloth.Outfit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OutfitRepository extends JpaRepository<Outfit, Long> {
+
+    @Query("select o from Outfit o where o.temp_from < :temp and o.temp_to > :temp and o.uv_from < :uv " +
+            "and o.uv_to > :uv and o.rain_from < :rain and o.rain_to > : rain")
+    List<Outfit> findAllOutifs(@Param(value = "temp") Long temp, @Param(value = "uv") Long uv, @Param(value = "rain") Long rain);
 }
