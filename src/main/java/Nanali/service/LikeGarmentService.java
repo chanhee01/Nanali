20b46716299cd69dc5bc522cb7ac8e84b0bec2f2
chanhee01 <joch4267@naver.com>
2023-10-28 21:06:10 +0900

    [Update] 엔티티 수정으로 인해 Garment의 url을 반환

--- src/main/java/Nanali/service/LikeGarmentService.java
+++ src/main/java/Nanali/service/LikeGarmentService.java
@@ -1,7 +1,7 @@
 package Nanali.service;
 
 import Nanali.domain.Member.Member;
-import Nanali.domain.outfit.LikeGarment;
+import Nanali.domain.cody.LikeClothes.LikeGarment;
 import Nanali.repository.LikeGarmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +17,11 @@ public class LikeGarmentService {
 
     private final LikeGarmentRepository likeGarmentRepository;
 
+    public Long save(LikeGarment likeGarment) {
+        LikeGarment save = likeGarmentRepository.save(likeGarment);
+        return save.getId();
+    }
+
     public LikeGarment findOne(Long id) {
         return likeGarmentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException());
     }
@@ -28,28 +33,28 @@ public class LikeGarmentService {
     public List<String> findTops(Member member) {
         List<LikeGarment> allTop = likeGarmentRepository.findAllTop(member);
 
-        List<String> LikeTops = allTop.stream().map(top -> top.getImgUrl()).collect(Collectors.toList());
+        List<String> LikeTops = allTop.stream().map(top -> top.getGarment().getImgUrl()).collect(Collectors.toList());
         return LikeTops;
     }
 
     public List<String> findPants(Member member) {
         List<LikeGarment> allPants = likeGarmentRepository.findAllPants(member);
 
-        List<String> LikePants = allPants.stream().map(pant -> pant.getImgUrl()).collect(Collectors.toList());
+        List<String> LikePants = allPants.stream().map(pant -> pant.getGarment().getImgUrl()).collect(Collectors.toList());
         return LikePants;
     }
 
     public List<String> findOuters(Member member) {
         List<LikeGarment> allOuter = likeGarmentRepository.findAllOuter(member);
 
-        List<String> LikeOuters = allOuter.stream().map(outer -> outer.getImgUrl()).collect(Collectors.toList());
+        List<String> LikeOuters = allOuter.stream().map(outer -> outer.getGarment().getImgUrl()).collect(Collectors.toList());
         return LikeOuters;
     }
 
     public List<String> findShoes(Member member) {
         List<LikeGarment> allShoes = likeGarmentRepository.findAllShoes(member);
 
-        List<String> LikeShoes = allShoes.stream().map(shoes -> shoes.getImgUrl()).collect(Collectors.toList());
+        List<String> LikeShoes = allShoes.stream().map(shoes -> shoes.getGarment().getImgUrl()).collect(Collectors.toList());
         return LikeShoes;
     }
 }
