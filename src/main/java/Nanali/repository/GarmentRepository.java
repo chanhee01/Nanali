ddeaa9b69476520b31765165414e8689384787dc
chanhee01 <joch4267@naver.com>
2023-10-29 16:08:45 +0900

    [Update] 기온, 강수량, 자외선 수치 범위에 등호 추가, Service 계층에서 구현

--- src/main/java/Nanali/repository/GarmentRepository.java
+++ src/main/java/Nanali/repository/GarmentRepository.java
@@ -9,20 +9,20 @@ import java.util.List;
 
 public interface GarmentRepository extends JpaRepository<Garment, Long> {
 
-    @Query("select g from Garment g where g.temp_from < :temp and g.temp_to > :temp and g.uv_from < :uv " +
-            "and g.uv_to > :uv and g.rain_from < :rain and g.rain_to > : rain and g.category = 'outer'")
+    @Query("select g from Garment g where g.temp_from <= :temp and g.temp_to >= :temp and g.uv_from <= :uv " +
+            "and g.uv_to >= :uv and g.rain_from <= :rain and g.rain_to >= : rain and g.category = 'outer'")
     List<Garment> findAllByCategoryIsOuter(@Param(value = "temp") Long temp, @Param(value = "uv") Long uv, @Param(value = "rain") Long rain);
 
-    @Query("select g from Garment g where g.temp_from < :temp and g.temp_to > :temp and g.uv_from < :uv " +
-            "and g.uv_to > :uv and g.rain_from < :rain and g.rain_to > : rain and g.category = 'top'")
+    @Query("select g from Garment g where g.temp_from <= :temp and g.temp_to >= :temp and g.uv_from <= :uv " +
+            "and g.uv_to >= :uv and g.rain_from <= :rain and g.rain_to >= : rain and g.category = 'top'")
     List<Garment> findAllByCategoryIsTop(@Param(value = "temp") Long temp, @Param(value = "uv") Long uv, @Param(value = "rain") Long rain);
 
-    @Query("select g from Garment g where g.temp_from < :temp and g.temp_to > :temp and g.uv_from < :uv " +
-            "and g.uv_to > :uv and g.rain_from < :rain and g.rain_to > : rain and g.category = 'pants'")
+    @Query("select g from Garment g where g.temp_from <= :temp and g.temp_to >= :temp and g.uv_from <= :uv " +
+            "and g.uv_to >= :uv and g.rain_from <= :rain and g.rain_to >= : rain and g.category = 'pants'")
     List<Garment> findAllByCategoryIsPants(@Param(value = "temp") Long temp, @Param(value = "uv") Long uv, @Param(value = "rain") Long rain);
 
-    @Query("select g from Garment g where g.temp_from < :temp and g.temp_to > :temp and g.uv_from < :uv " +
-            "and g.uv_to > :uv and g.rain_from < :rain and g.rain_to > : rain and g.category = 'shoes'")
+    @Query("select g from Garment g where g.temp_from <= :temp and g.temp_to >= :temp and g.uv_from <= :uv " +
+            "and g.uv_to >= :uv and g.rain_from <= :rain and g.rain_to >= : rain and g.category = 'shoes'")
     List<Garment> findAllByCategoryIsShoes(@Param(value = "temp") Long temp, @Param(value = "uv") Long uv, @Param(value = "rain") Long rain);
 
 }
--- src/main/java/Nanali/service/GarmentService.java
+++ src/main/java/Nanali/service/GarmentService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -17,5 +19,21 @@ public class GarmentService {
     public Long save(Garment garment) {
         Garment save = garmentRepository.save(garment);
         return save.getId();
+    }
+
+    public List<Garment> findOuters(Long temp, Long uv, Long rain) {
+        return garmentRepository.findAllByCategoryIsOuter(temp, uv, rain);
+    }
+
+    public List<Garment> findTops(Long temp, Long uv, Long rain) {
+        return garmentRepository.findAllByCategoryIsTop(temp, uv, rain);
+    }
+
+    public List<Garment> findPants(Long temp, Long uv, Long rain) {
+        return garmentRepository.findAllByCategoryIsPants(temp, uv, rain);
+    }
+
+    public List<Garment> findShoes(Long temp, Long uv, Long rain) {
+        return garmentRepository.findAllByCategoryIsShoes(temp, uv, rain);
     }
 }
