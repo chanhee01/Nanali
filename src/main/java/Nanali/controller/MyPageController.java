8204a64791386df34172cdfc6c121d7f49672101
chanhee01 <joch4267@naver.com>
2023-11-01 13:02:12 +0900

    [Update] likeOutfit 조회 컨트롤러 수정, user 정보 수정 컨트롤러 추가

--- src/main/java/Nanali/controller/MyPageController.java
+++ src/main/java/Nanali/controller/MyPageController.java
@@ -1,15 +1,14 @@
 package Nanali.controller;
 
 import Nanali.domain.Member.Member;
+import Nanali.domain.Member.Style;
 import Nanali.dtos.MyPage.GarmentDto;
 import Nanali.dtos.MyPage.OutfitDto;
 import Nanali.service.LikeGarmentService;
 import Nanali.service.LikeOutfitService;
 import Nanali.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,28 +21,52 @@ public class MyPageController {
     private final LikeGarmentService likeGarmentService;
     private final LikeOutfitService likeOutfitService;
 
-    @GetMapping("/garment")
-    public GarmentDto garment() {
-        Member member = memberService.findMember(1L);
+    @GetMapping("/outfit")
+    public OutfitDto outfit() {
+        Member member = memberService.findById(1L);
         String nickname = member.getNickname();
         String email = member.getEmail();
+        String memberImg = member.getMemberImg().getImgUrl();
+
+        List<String> outfits = likeOutfitService.findAllByMember(member);
+
+        OutfitDto outfitDto = new OutfitDto(nickname, email, memberImg, outfits);
+        return outfitDto;
+    }
+
+    @GetMapping("/garment")
+    public GarmentDto garment() {
+        Member member = memberService.findById(1L);
+        String nickname = member.getNickname();
+        String email = member.getEmail();
+        String memberImg = member.getMemberImg().getImgUrl();
+
         List<String> tops = likeGarmentService.findTops(member);
         List<String> pants = likeGarmentService.findPants(member);
         List<String> outers = likeGarmentService.findOuters(member);
         List<String> shoes = likeGarmentService.findShoes(member);
 
-        GarmentDto garmentDto = new GarmentDto(nickname, email, tops, pants, outers, shoes);
+        GarmentDto garmentDto = new GarmentDto(nickname, email, memberImg, tops, pants, outers, shoes);
         return garmentDto;
     }
 
-    @GetMapping("/outfit")
-    public OutfitDto outfit() {
-        Member member = memberService.findMember(1L);
-        String nickname = member.getNickname();
-        String email = member.getEmail();
-        List<String> outfits = likeOutfitService.findAllByMember(member);
+    // 로그인 시 세션 넣고 인증도 넣어야할듯
 
-        OutfitDto outfitDto = new OutfitDto(nickname, email, outfits);
-        return outfitDto;
+    @PutMapping("/change/nickname")
+    public void changeNickname(@RequestBody String nickname) {
+        Member member = memberService.findById(1L);
+        memberService.changeNickname(member, nickname);
+    }
+
+    @PutMapping("/change/style")
+    public void changeStyle(@RequestBody Style style) {
+        Member member = memberService.findById(1L);
+        memberService.changeStyle(member, style);
+    }
+
+    @PutMapping("/change/password")
+    public void changePassword(@RequestBody String password) {
+        Member member = memberService.findById(1L);
+        memberService.changePassword(member, password);
     }
 }
