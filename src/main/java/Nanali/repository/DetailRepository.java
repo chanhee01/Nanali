c50b6aa8ff56ba2c86e0507226fe63b09ebebbbe
chanhee01 <joch4267@naver.com>
2023-11-09 13:53:51 +0900

    [Fix] Detail에서 garment가 아니라 outfit과 join 관계

--- src/main/java/Nanali/repository/DetailRepository.java
+++ src/main/java/Nanali/repository/DetailRepository.java
@@ -7,15 +7,15 @@ import org.springframework.data.repository.query.Param;
 
 public interface DetailRepository extends JpaRepository<Detail, Long> {
 
-    @Query("select d from Detail d join d.garment g where g.id = :id and d.category = 'OUTER'")
+    @Query("select d from Detail d join d.outfit o where o.id = :id and d.category = 'OUTER'")
     Detail findOuter(@Param(value = "id") Long id);
 
-    @Query("select d from Detail d join d.garment g where g.id = :id and d.category = 'TOP'")
+    @Query("select d from Detail d join d.outfit o where o.id = :id and d.category = 'TOP'")
     Detail findTop(@Param(value = "id") Long id);
 
-    @Query("select d from Detail d join d.garment g where g.id = :id and d.category = 'PANTS'")
+    @Query("select d from Detail d join d.outfit o where o.id = :id and d.category = 'PANTS'")
     Detail findPants(@Param(value = "id") Long id);
 
-    @Query("select d from Detail d join d.garment g where g.id = :id and d.category = 'SHOES'")
+    @Query("select d from Detail d join d.outfit o where o.id = :id and d.category = 'SHOES'")
     Detail findShoes(@Param(value = "id") Long id);
 }
