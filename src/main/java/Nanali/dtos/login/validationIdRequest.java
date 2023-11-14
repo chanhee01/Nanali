5366e0fcd5a81aa5ebeddb4ed3672563267f1a03
chanhee01 <joch4267@naver.com>
2023-11-14 23:18:43 +0900

    [Add] 회원가입 시 아이디, 닉네임 중복 체크

---
+++ src/main/java/Nanali/dtos/login/validationIdRequest.java
@@ -0,0 +1,12 @@
+package Nanali.dtos.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class validationIdRequest {
+    private String loginId;
+}
---
+++ src/main/java/Nanali/dtos/login/validationNicknameRequest.java
@@ -0,0 +1,12 @@
+package Nanali.dtos.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class validationNicknameRequest {
+    private String nickname;
+}
