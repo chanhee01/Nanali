108041fcc2ac4825c08bc1010e010e2b960912ff
chanhee01 <joch4267@naver.com>
2023-11-15 00:12:56 +0900

    [Update] 로그인 관련 DTO에 검증 조건 넣기

--- src/main/java/Nanali/dtos/login/validationIdRequest.java
+++ src/main/java/Nanali/dtos/login/validationIdRequest.java
@@ -1,5 +1,6 @@
 package Nanali.dtos.login;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class validationIdRequest {
+    @NotBlank
     private String loginId;
 }
